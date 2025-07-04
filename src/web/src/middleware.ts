d13f37a912c875466b67b20c931a19e529d1cb74
jbcarreon123 <jbcarreon212@gmail.com>
2025-07-04 16:07:37 +0800

    caching now works yay

--- src/web/src/middleware.ts
+++ src/web/src/middleware.ts
@@ -2,6 +2,9 @@ import type { APIRoute, MiddlewareHandler } from "astro";
 
 export const onRequest: MiddlewareHandler = async (context, next) => {
     const response = await next();
+    if (!context.url.pathname.startsWith('/c/')) {
+      response.headers.append('Content-Security-Policy', `script-src 'self' https://challenges.cloudflare.com; object-src 'none'; frame-ancestors 'self';`)
+    }
     if (response.status === 404 && response.headers.get('Content-Type') !== 'text/html') {
       return context.rewrite("/404");
     }
