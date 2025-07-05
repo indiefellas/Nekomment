394def53aac123fe66eeb94ddd3ec18461e803b4
jbcarreon123 <jbcarreon212@gmail.com>
2025-07-05 14:56:13 +0800

    fix syntax error

--- src/api/src/services/internals.ts
+++ src/api/src/services/internals.ts
@@ -505,7 +505,7 @@ export class InternalService extends WorkerEntrypoint {
     website: string | null;
     createdAt: Date | null;
     parentId: string | null;
-    approved: boolean | null;p
+    approved: boolean | null;
     moderatedBy: string | null;
     replies: {
       id: string;
