08373ffe0df6ec2f6b1c9f6bc1f9aa5101e006ae
jbcarreon123 <jbcarreon212@gmail.com>
2025-07-08 23:28:20 +0800

    add a link button

---
+++ src/web/src/components/buttons/LinkButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Tooltip from "../ui/Tooltip.svelte";
+
+    const buttonCode = `<a href="https://beta.cmt.nkko.link/"><img src="https://beta.cmt.nkko.link/res/88x31x2.png" alt="Powered by Nekomment"></a>`;
+
+    let copied = false;
+    let copiedTimeout = 0;
+
+    let tooltip: Tooltip;
+
+    function copyCode() {
+        navigator.clipboard.writeText(buttonCode);
+
+        copied = true;
+        tooltip.calculatePositions();
+
+        clearTimeout(copiedTimeout);
+        copiedTimeout = setTimeout(() => {
+            copied = false;
+            tooltip.calculatePositions();
+        }, 2000) as unknown as number;
+    }
+</script>
+
+<Tooltip bind:this={tooltip}>
+    <button slot="content" on:click={() => copyCode()}>
+        <img src="/res/88x31x2.png" alt="Powered by Nekomment">
+    </button>
+    <span slot="tooltip">
+        {copied ? "Button code copied!" : "Click to copy button code"}
+    </span>
+</Tooltip>
