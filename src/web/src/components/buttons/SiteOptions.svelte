f640e9c71e9ab58c6fca108ebf03f7427e6f7cad
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-07-10 20:13:11 +0700

    adjust site options panel

--- src/web/src/components/buttons/SiteOptions.svelte
+++ src/web/src/components/buttons/SiteOptions.svelte
@@ -28,20 +28,22 @@
 
         .options {
             position: absolute;
-            top: 75%;
+            top: 100%;
+            transform: scale(0.8);
+            transform-origin: top right;
             right: 0;
             width: 150px;
-            background-color: var(--background-1);
+            background-color: var(--background-3);
             border-radius: 3px;
-            padding: 10px;
+            padding: 0.5em 1.2ch;
             display: flex;
             flex-direction: column;
-            margin-top: 10px;
+            margin-block: 5px;
             gap: 10px;
             opacity: 0;
             pointer-events: none;
             box-shadow: var(--shadow-2);
-
+            text-align: end;
             a {
                 text-decoration: none;
             }
@@ -49,13 +51,16 @@
             &:global(.visible) {
                 opacity: 1;
                 pointer-events: all;
-                top: 100%;
-
-                @media screen and (width <= 640px) {
-                    margin-top: 20px;
-                    top: calc(-200% - 20px);
-                }
+                transform: none;
             }
+        }
+    }
+
+    @media screen and (width <= 640px) {
+        .site-options .options {
+            top: auto;
+            bottom: 100%;
+            transform-origin: bottom right;
         }
     }
 </style>
